72a8bcf8391148b7aa281c267c332672d28a7bc1
Scott Stringer <scott_ts@hotmail.com>
2021-04-15 12:05:23 -0500

    completed

---
+++ StaticExercise/StaticExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(50);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(30);
+
+            Console.WriteLine($"50 degrees Fahrenheit is {celsius} Celcius."); // should be 10
+            Console.WriteLine($"30 degrees Celcius is {fahrenheit} Fahrenheit."); // should be 86
+        }
+    }
+}
